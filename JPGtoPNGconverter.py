e471056a698a774bde63160857656ba4918ca738
Akshay kumar <akshay1.kumar@lntinfotech.com>
2021-07-06 00:09:08 +0530

    Initial commit

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import sys
+import os
+
+#grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+#check if new exist? if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+#loop through pokedrox
+for file in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{file}')
+    clean_name = os.path.splitext(file)[0]
+    img.save(f'{output_folder}{clean_name}.png','png')
+    print('All done!')
